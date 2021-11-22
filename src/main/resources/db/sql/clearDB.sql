f42177a4645de6e08e30bcafed9976e342aca2c8
dvur0g <hoof7@mail.ru>
2021-11-22 03:31:03 +0300

    refactor clearDB script

--- src/main/resources/db/sql/clearDB.sql
+++ src/main/resources/db/sql/clearDB.sql
@@ -1,7 +1,7 @@
-DELETE FROM cards.game_players;
-DELETE FROM cards.game;
-DELETE FROM cards.player_cards;
-DELETE FROM cards.player;
+TRUNCATE TABLE cards.game_players;
+TRUNCATE TABLE cards.game;
+TRUNCATE TABLE cards.player_cards;
+TRUNCATE TABLE  cards.player;
 
 ALTER SEQUENCE cards.seq_game RESTART;
 ALTER SEQUENCE cards.seq_player RESTART;
